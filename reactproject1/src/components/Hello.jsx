2e3319f4dddbe00d9212189d76172716fa03ee78
MlalaWilliem <mlalawilliam@gamil.com>
2025-09-25 06:45:01 +0200

    commit1

---
+++ reactproject1/src/components/Hello.jsx
@@ -0,0 +1,12 @@
+function Hello() {
+    return <div >
+
+        <h1>HELLO WORLD</h1>
+        <h3>In  this app we will be learning how to build responsive websites</h3>
+
+    </div>;
+
+
+}
+
+export default Hello;
